fa8c80f3bd4b89b7a33281c3ed4d6245bb6d35b0
Iuri Chiba <iurichiba@gmail.com>
2020-03-16 15:14:48 -0300

    Remote installation fixed (colors.sh reference doesn't work)

--- remote-installation.sh
+++ remote-installation.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-SPATH=${BASH_SOURCE%/*}
-source "${SPATH}/helpers/colors.sh"
-
+set -e
+BOLD="\033[1m"
+RESET="\033[0m"
 MSETUPGIT="https://github.com/iurichiba/msetup.git"
 MSETUPPATH="$HOME/.msetup"
 
@@ -9,7 +9,7 @@ if [ ! -d $MSETUPPATH ]; then
     echo "${BOLD}Running the setup script...${RESET}"
     git clone --depth=1 $MSETUPGIT $MSETUPPATH
     cd $MSETUPPATH
-    ${SPATH}/install.sh
+    ./install.sh
 else
     echo "${BOLD}.msetup is cloned already. Try running install.sh from ${MSETUPPATH}${RESET}"
 fi
