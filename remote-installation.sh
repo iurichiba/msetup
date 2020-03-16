7c44c19fcda681f39171dda98b4c71f7302794f8
Iuri Chiba <iurichiba@gmail.com>
2020-03-16 19:01:21 -0300

    Remote installation w/ support for "updates"

--- remote-installation.sh
+++ remote-installation.sh
@@ -2,16 +2,20 @@
 set -e
 RED="\033[31m"
 BOLD="\033[1m"
+CYAN="\033[36m"
 RESET="\033[0m"
 MSETUPGIT="https://github.com/iurichiba/msetup.git"
 MSETUPPATH="$HOME/.msetup"
 
 if [ ! -d $MSETUPPATH ]; then
-    echo "${BOLD}Running the setup script...${RESET}"
+    echo "\n${BOLD}Downloading the script into your machine...${RESET}"
     git clone --depth=1 $MSETUPGIT $MSETUPPATH
-    cd $MSETUPPATH
-    ./install.sh
+    echo "\n${CYAN}Running script...${RESET}"
+    cd $MSETUPPATH && ./install.sh
 else
-    echo "${RED}🚫 .msetup is cloned already. Try running ${BOLD}install.sh from ${MSETUPPATH}${RESET}"
-    echo "⚠️  You might want to git pull again, btw."
+	echo "${RED}⚠️  .msetup already exists!${RESET}"
+	echo "\n${BOLD}Pulling last version from GitHub...${RESET}"
+    cd $MSETUPPATH && git pull
+    echo "\n${CYAN}Running script...${RESET}"
+    ./install.sh
 fi
