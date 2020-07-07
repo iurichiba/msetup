d84ed82142c0885ff9d12012a2099815672a9c43
Iuri Chiba <iurichiba@gmail.com>
2020-07-07 16:58:05 -0300

    Sublime bin link fixed

--- scripts/customizations/sublime-bin.sh
+++ scripts/customizations/sublime-bin.sh
@@ -5,6 +5,6 @@ if type "sublime" > /dev/null; then
 	echo "${GREEN}💡 Sublime Text alias is already configured!${RESET}"
 else
 	echo "Setting up ${BOLD}Sublime Text alias${RESET}..."
-	ln -s "~/Applications/Development Tools/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/sublime
+	ln -s ~/Applications/Homebrewed/dev_tools/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/sublime
 	ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/sublime
 fi
